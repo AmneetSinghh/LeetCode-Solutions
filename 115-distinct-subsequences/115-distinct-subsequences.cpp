df8659552065e2a5dc5c82c39db7e7992c932565
Amneet Singh <37026409+AmneetSinghh@users.noreply.github.com>
2022-08-02 10:01:44 +0530

    Time: 27 ms (92.29%), Space: 6.5 MB (82.56%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -13,23 +13,41 @@ public:
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        // vector< vector<int>> dp(n+1,vector<int>(m+1,-1));
-        // return dfs(n,m,s,t,dp);
-        vector< vector<long>> dp(n+1,vector<long>(m+1,0));
-        for(int i=0;i<=n;i++)dp[i][0]=1;// j ==0 means, when all t th string is empty
+//         // vector< vector<int>> dp(n+1,vector<int>(m+1,-1));
+//         // return dfs(n,m,s,t,dp);
+//         vector< vector<long>> dp(n+1,vector<long>(m+1,0));
+//         for(int i=0;i<=n;i++)dp[i][0]=1;// j ==0 means, when all t th string is empty
         
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=m;j++){
+//                 if(s[i-1]==t[j-1]){
+//                      dp[i][j]=(int)(dp[i-1][j-1]+dp[i-1][j]);
+//                 }
+//                 else{
+//                     dp[i][j]=dp[i-1][j];
+//                 } 
+//             }
+//         }
+        
+//         return (int)dp[n][m];
+        
+        
+        
+//**********************  space optimation  ******************************
+        vector<long> prev(m+1,0),cur(m+1,0);
+        prev[0]=cur[0]=1;        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s[i-1]==t[j-1]){
-                     dp[i][j]=(int)(dp[i-1][j-1]+dp[i-1][j]);
+                     cur[j]=(int)(prev[j-1]+prev[j]);
                 }
                 else{
-                    dp[i][j]=dp[i-1][j];
+                    cur[j]=prev[j];
                 } 
             }
+            prev=cur;
         }
-        
-        return (int)dp[n][m];
+        return prev[m];
         
         
     }
