bcbdcb05e92efe40c255f6e2fb6f445cea614819
Amneet Singh <37026409+AmneetSinghh@users.noreply.github.com>
2022-08-02 15:45:04 +0530

    Time: 10 ms (80.34%), Space: 12.7 MB (43.41%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -5,8 +5,8 @@ public:
         int n = a.size();
         vector< vector<int>> dp(n+1,vector<int>(2*k+12,0));
         
-        for(int i=n-1;i>=0;i--){
-            for(int j=2*k-1;j>=0;j--){
+        for(int i=n-1;i>=0;i--){// oppposite to memorisation.
+            for(int j=2*k-1;j>=0;j--){// oppposite to memorisation.
                 if(j%2==0) dp[i][j]=max(-a[i]+dp[i+1][j+1],dp[i+1][j]);
                 else{
                     dp[i][j]=max(a[i]+dp[i+1][j+1],dp[i+1][j]);
