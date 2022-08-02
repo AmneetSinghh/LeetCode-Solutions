3e8d62f153d1a88524f889be6d8a8a8cf153aa83
Amneet Singh <37026409+AmneetSinghh@users.noreply.github.com>
2022-08-02 13:47:52 +0530

    Time: 19 ms (20.16%), Space: 12.9 MB (89.51%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -21,23 +21,46 @@ public:
         
         
         // memorization.
+    
+//         vector<vector<int>> dp(n+1,vector<int>(2,0));
+//         dp[n][0]=0,dp[n][1]=0;
+//         for(int i=n-1;i>=0;i--){
+//             for(int buy=0;buy<=1;buy++){
+//                 long profit=0;
+//                 if(buy){
+//                     profit=max(-a[i]+dp[i+1][0],dp[i+1][1]);// buy or not buy
+//                 }
+//                 else{
+//                     profit=max(a[i]+dp[i+1][1],dp[i+1][0]);// sell or not sell
+//                 }
+//                 dp[i][buy]=profit;
+//             }
+//         }
         
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
-        dp[n][0]=0,dp[n][1]=0;
+//         return dp[0][1];// buy.
+        
+        
+        
+        
+        
+        // simple 2 array 1d approach
+        vector<int> prev(2,0),cur(2,0);
+        prev[0]=0,prev[1]=0;
         for(int i=n-1;i>=0;i--){
             for(int buy=0;buy<=1;buy++){
                 long profit=0;
                 if(buy){
-                    profit=max(-a[i]+dp[i+1][0],dp[i+1][1]);// buy or not buy
+                    profit=max(-a[i]+prev[0],prev[1]);// buy or not buy
                 }
                 else{
-                    profit=max(a[i]+dp[i+1][1],dp[i+1][0]);// sell or not sell
+                    profit=max(a[i]+prev[1],prev[0]);// sell or not sell
                 }
-                dp[i][buy]=profit;
+                cur[buy]=profit;
             }
+            prev=cur;
         }
         
-        return dp[0][1];// buy.
+        return prev[1];// buy.
 
     }
 };
