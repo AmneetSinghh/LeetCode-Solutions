87c2395fe75e14dcab0a242c60b10f0a64f63a5c
Amneet Singh <37026409+AmneetSinghh@users.noreply.github.com>
2022-09-07 22:44:52 +0530

    Time: 17 ms (14.43%), Space: 14 MB (74.52%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        while(l <= r)
+        {
+            int mid = (l+r)/2;
+            if (nums[mid] == target)
+                return true;
+			// with duplicates we can have this contdition, just update left & right
+            if((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
+            {
+                l++;
+                r--;
+            }
+            else if(nums[l] <= nums[mid])
+            {
+                if (nums[mid]>=target and nums[l]<=target)
+                    r=mid-1;
+                else
+                    l=mid+1;
+            }
+            else
+            {
+                if(target>=nums[mid] and target<=nums[r])
+                    l=mid+1;
+                else
+                    r=mid-1;
+            }
+        }
+        return false;
+    }
+};
