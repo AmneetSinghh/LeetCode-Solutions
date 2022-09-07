164133355f5d0ff516f7c9c5f635e52c72973ffc
Amneet Singh <37026409+AmneetSinghh@users.noreply.github.com>
2022-09-08 01:14:06 +0530

    Time: 16 ms (14.25%), Space: 12.3 MB (18.96%) - LeetHub

--- 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -7,10 +7,7 @@ public:
         int ans=a[0];
         while(l<=r){
             int mid = (l+r)/2;
-            cout<<l<<" "<<r<<" "<<mid<<"\n";
             if(mid-1>=0 and mid+1<a.size() and  a[mid-1]>a[mid] and a[mid]<a[mid+1])return a[mid];
-            // if(mid-1<0 and a[mid]<a[mid+1])return a[mid];
-            // if(mid+1>=a.size() and a[mid-1]>a[mid])return a[mid];
             ans = min(ans,a[mid]);
             if(a[mid]>a[r])l=mid+1;
             else if(a[mid]<a[r])r=mid-1;
